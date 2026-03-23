2b24bbca8802536ad108e102ddb906fa52f0fd0a
Renan <renanlopesteles@gmail.com>
2026-03-23 19:00:32 -0300

    Modificações e atualizações nos arquivos sql

--- sql/02_seed_basic.sql
+++ sql/02_seed_basic.sql
@@ -1,9 +1,15 @@
 USE strong_memory_db;
 
-INSERT IGNORE INTO difficults (difficulty) VALUES
-('easy'), 
-('normal'), 
-('hard');
+INSERT IGNORE INTO difficults (
+	difficulty, 
+    translation, 
+    max_quantity_words, 
+    increase_display_time_seconds, 
+    increase_typing_time_seconds
+) VALUES
+('easy', 'fácil', 15, 5, 5), 
+('normal', 'normal', 25, 3, 3), 
+('hard', 'difícil', 40, 2, 2);
 
 INSERT IGNORE INTO user_roles(role) VALUES 
 ('ROLE_PLAYER'),
